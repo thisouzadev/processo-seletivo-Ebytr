64e30f95866e0519d5df71f059f4beb498601891
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-02-17 09:44:56 -0300

    fix: delete gitignore

--- src/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-
-http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
-});
